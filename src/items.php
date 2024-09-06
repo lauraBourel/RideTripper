badcbcbc730212cb646873bf86e634d2e29e2b29
laura.bourel <you@example.com>
2024-09-06 16:54:12 +0200

    ajout items page

---
+++ src/items.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/items.css">
+    <title>Equipement Moto</title>
+</head>
+
+<body class="scroller">
+    <?php require './ui/navbar.php' ?>
+    <main>
+        <h1>Equipement Moto</h1>
+        <div class="itemsBox">
+            <div class="itemCard"></div>
+            <div class="itemCard"></div>
+            <div class="itemCard"></div>
+            <div class="itemCard"></div>
+            <div class="itemCard"></div>
+            <div class="itemCard"></div>
+        </div>
+
+    </main>
+
+
+
+
+
+
+
+
+
+
+
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
